bbe82c60abf77a23a41dce57481d260963dca244
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2021-02-28 14:34:28 -0300

    creating context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { CompleteChallenges } from '../components/CompleteChallenges';
 import { Countdown } from '../components/CountDown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import React from "react";
 
 export default function Home() {
   return (
@@ -23,7 +25,7 @@ export default function Home() {
         <Countdown />
         </div>
         <div>
-
+        <ChallengeBox />
         </div>
       </section>
     </div>
